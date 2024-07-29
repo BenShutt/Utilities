366cf16c4080fdbe49b9dc450684df6626c26def
Ben Shutt <ben.c.shutt@gmail.com>
2024-07-29 17:03:09 +0100

    Update BlurView.swift

--- Sources/Utilities/SwiftUI/BlurView.swift
+++ Sources/Utilities/SwiftUI/BlurView.swift
@@ -11,14 +11,24 @@ import SwiftUI
 #if os(iOS)
 
 /// A `UIViewRepresentable` wrapping a `UIVisualEffectView`
-struct BlurView: UIViewRepresentable {
-    var style: UIBlurEffect.Style = .regular
+/// with a `UIBlurEffect`
+public struct BlurView: UIViewRepresentable {
+    public var style: UIBlurEffect.Style = .regular
 
-    func makeUIView(context: Context) -> UIVisualEffectView {
-        UIVisualEffectView(effect: UIBlurEffect(style: style))
+    public init(style: UIBlurEffect.Style) {
+        self.style = style
     }
 
-    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {}
+    public func makeUIView(context: Context) -> UIVisualEffectView {
+        UIVisualEffectView(
+            effect: UIBlurEffect(style: style)
+        )
+    }
+
+    public func updateUIView(
+        _ uiView: UIVisualEffectView,
+        context: Context
+    ) {}
 }
 
 #endif
