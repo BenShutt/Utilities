c1be424eb8e63463f5da0ee529f30b735119a8ec
Ben Shutt <ben.c.shutt@gmail.com>
2024-07-29 17:03:07 +0100

    Merge View extensions

--- Sources/Utilities/SwiftUI/View+Extensions.swift
+++ Sources/Utilities/SwiftUI/View+Extensions.swift
@@ -19,4 +19,35 @@ public extension View {
             alignment: alignment
         )
     }
+
+    func onSizeChange(
+        _ action: @escaping (CGSize) -> Void
+    ) -> some View {
+        background(
+            GeometryReader { proxy in
+                Color.clear.preference(
+                    key: SizePreferenceKey.self,
+                    value: proxy.size
+                )
+            }
+        )
+        .onPreferenceChange(SizePreferenceKey.self) { value in
+            action(value)
+        }
+    }
+
+    func onWidthChange(_ binding: Binding<CGFloat>) -> some View {
+        onSizeChange { binding.wrappedValue = $0.width }
+    }
+
+    func onHeightChange(_ binding: Binding<CGFloat>) -> some View {
+        onSizeChange { binding.wrappedValue = $0.height }
+    }
+}
+
+// MARK: - SizePreferenceKey
+
+public struct SizePreferenceKey: PreferenceKey {
+    public static var defaultValue: CGSize = .zero
+    public static func reduce(value: inout CGSize, nextValue: () -> CGSize) {}
 }
