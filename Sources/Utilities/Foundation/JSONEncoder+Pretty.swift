135b4582c3284cfb9d1411f412cd081397c33909
Ben Shutt <ben.c.shutt@gmail.com>
2024-07-29 17:01:17 +0100

    Add JSONEncoder+Pretty

---
+++ Sources/Utilities/Foundation/JSONEncoder+Pretty.swift
@@ -0,0 +1,22 @@
+//
+//  JSONEncoder+Pretty.swift
+//  Utilities
+//
+//  Created by Ben Shutt on 29/07/2024.
+//  Copyright © 2024 Ben Shutt. All rights reserved.
+//
+
+import Foundation
+
+public extension JSONEncoder {
+    static let pretty = {
+        let encoder = JSONEncoder()
+        encoder.dateEncodingStrategy = .iso8601
+        encoder.outputFormatting = [
+            .prettyPrinted,
+            .sortedKeys,
+            .withoutEscapingSlashes
+        ]
+        return encoder
+    }()
+}
